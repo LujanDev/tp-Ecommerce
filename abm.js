00e358f5cd6ee206841132a815f528f5ce8e23c2
LujanDev <yani828@outlook.com>
2023-07-04 22:42:57 -0300

    Creando ABM

---
+++ abm.js
@@ -0,0 +1,23 @@
+/**Formulario ABM**/
+let formulario=document.getElementById('formABM');
+let personasList=document.getElementById('personasList');
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    //Obtener los valores de los campos del formulario
+    let fotoProd=document.getElementById('foto-prod').value;
+    let nombreProd=document.getElementById('nombre-prod').value;
+    let detalleProd=document.getElementById('detalle-prod').value;
+
+    //crear un li con los datos
+
+    let listItem= document.createElement('li');
+    listItem.innerHTML=fotoProd+' '+nombreProd+' '+detalleProd;
+
+    //Agregar el elemento de la lista a la lista de personas
+    personasList.appendChild(listItem);
+
+    formulario.reset();
+
+});
